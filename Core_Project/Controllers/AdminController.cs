8857efd77b52a54a62fc930d4cd8d21d0a0984de
Esra <elifesraeker@gmail.com>
2022-12-27 10:00:23 +0300

    Güncellendi

---
+++ Core_Project/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.Controllers
+{
+    public class AdminController : Controller
+    {
+        public PartialViewResult PartialSideBar()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ Core_Project/Controllers/FeatureController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.Controllers
+{
+    public class FeatureController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
