2a77a79d526d96e24fbe9c66875ec9cc784dab32
Esra <elifesraeker@gmail.com>
2023-01-24 17:38:55 +0300

    Deneyimlerin listelenmesi

---
+++ Core_Project/Controllers/ExperienceController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
+
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Deneyim Ekleme";
+            ViewBag.v2 = "Deneyimler";
+            ViewBag.v3 = "Deneyim Ekleme";
+            var values = experienceManager.TGetList();
+            return View(values);
+        }
+    }
+}
