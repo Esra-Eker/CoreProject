260d85f810132abf8f724ee0615b6725a173c2c2
Esra <elifesraeker@gmail.com>
2023-01-27 17:41:07 +0300

    Feature Düzenlemeleri

--- Core_Project/Controllers/FeatureController.cs
+++ Core_Project/Controllers/FeatureController.cs
@@ -1,12 +1,60 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Project.Controllers
 {
     public class FeatureController : Controller
     {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            ViewBag.v1 = "Düzenleme";
+            ViewBag.v2 = "Öne Çıkanlar";
+            ViewBag.v3 = "Öne Çıkan Sayfası";
+            var values = featureManager.TGetByID(1);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Feature feature)
+        {
+            featureManager.TUpdate(feature);
+            return RedirectToAction("Index","Default");
+        }
+
+        [HttpPost]
+        public IActionResult AddSkill(Feature skill)
+        {
+            featureManager.TAdd(skill);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteSkill(int id)
+        {
+            var values = featureManager.TGetByID(id);
+            featureManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditSkill(int id)
+        {
+            ViewBag.v1 = "Yetenek Güncelleme";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Güncelleme";
+            var values = featureManager.TGetByID(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult EditSkill(Feature skill)
+        {
+            featureManager.TUpdate(skill);
+            return RedirectToAction("Index");
         }
     }
 }
