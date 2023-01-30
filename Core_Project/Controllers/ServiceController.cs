8fbda4cd7c8c222cc3561d65d60ee86c90877c06
Esra <elifesraeker@gmail.com>
2023-01-30 11:41:06 +0300

    Service listesi

---
+++ Core_Project/Controllers/ServiceController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.Controllers
+{
+    public class ServiceController : Controller
+    {
+        ServiceManager serviceManager = new ServiceManager(new EfServiceDal());
+
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Hizmet Listesi";
+            ViewBag.v2 = "Hizmetler";
+            ViewBag.v3 = "Hizmet Listesi";
+            var values = serviceManager.TGetList();
+            return View(values);
+        }
+    }
+}
