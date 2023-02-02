715685f9adaca92ed0765b82f1d73ed4e9804a81
Esra <elifesraeker@gmail.com>
2023-02-02 15:32:59 +0300

    Dashboard FeatureStatistics kısmı

---
+++ Core_Project/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.ViewComponents.Dashboard
+{
+    public class FeatureStatistics : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            //var values = contactManager.TGetList();
+            return View();
+        }
+    }
+}
