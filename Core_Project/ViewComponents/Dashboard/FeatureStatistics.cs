2213330b56f4826a7f39a0d5dc51651c5f46e6fe
Esra <elifesraeker@gmail.com>
2023-02-02 18:05:23 +0300

    İstatistiklerin yazılması

--- Core_Project/ViewComponents/Dashboard/FeatureStatistics.cs
+++ Core_Project/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -1,4 +1,6 @@
-﻿using BusinessLayer.Concrete;
+﻿using System.Linq;
+using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,10 +8,17 @@ namespace Core_Project.ViewComponents.Dashboard
 {
     public class FeatureStatistics : ViewComponent
     {
+        private Context c = new Context();
+
         public IViewComponentResult Invoke()
         {
-            //var values = contactManager.TGetList();
+            ViewBag.v1 = c.Skills.Count();
+            ViewBag.v2 = c.Messages.Where(x => x.Status == false).Count();
+            ViewBag.v3 = c.Messages.Where(x => x.Status == true).Count();
+            ViewBag.v4 = c.Experiences.Count();
             return View();
         }
     }
 }
+
+
