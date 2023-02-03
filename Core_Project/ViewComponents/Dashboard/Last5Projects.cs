00964892198c80c8ffe020019b7350081fa2863e
Esra <elifesraeker@gmail.com>
2023-02-03 10:21:21 +0300

    Son 5 proje listesi

---
+++ Core_Project/ViewComponents/Dashboard/Last5Projects.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.ViewComponents.Dashboard
+{
+    public class Last5Projects : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
