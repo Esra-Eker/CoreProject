dd9e64e8eb05733b536ca54c7247704d85392493
Esra <elifesraeker@gmail.com>
2023-02-03 17:37:19 +0300

    Dashboard mesaj paneli

---
+++ Core_Project/ViewComponents/Dashboard/MessageList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.ViewComponents.Dashboard
+{
+    public class MessageList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
