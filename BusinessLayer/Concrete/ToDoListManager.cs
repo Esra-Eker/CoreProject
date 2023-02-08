9922075053706d91f028ca7e04bea7d61afebf2f
Esra <elifesraeker@gmail.com>
2023-02-08 16:45:14 +0300

    ToDoList entitysi eklendi ve listelendi

---
+++ BusinessLayer/Concrete/ToDoListManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class ToDoListManager : IToDoListService
+    {
+        private IToDoListDal _toDoListDal;
+
+        public ToDoListManager(IToDoListDal toDoListDal)
+        {
+            _toDoListDal = toDoListDal;
+        }
+        
+        public void TAdd(ToDoList t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(ToDoList t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(ToDoList t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ToDoList> TGetList()
+        {
+            return _toDoListDal.GetList();
+        }
+
+        public ToDoList TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
