5f39599d6883a140395525202f1e89c10784b91a
Esra <elifesraeker@gmail.com>
2023-02-08 16:57:24 +0300

    VisitorMap eklendi

---
+++ Core_Project/ViewComponents/Dashboard/VisitorMap.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.ViewComponents.Dashboard
+{
+    public class VisitorMap :ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
