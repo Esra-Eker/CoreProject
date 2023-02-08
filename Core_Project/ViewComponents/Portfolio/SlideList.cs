61b8f2a771b617332818b7a7438cc2cae8934f49
Esra <elifesraeker@gmail.com>
2023-02-08 12:18:07 +0300

    Slide paneli

---
+++ Core_Project/ViewComponents/Portfolio/SlideList.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.ViewComponents.Portfolio
+{
+    public class SlideList : ViewComponent
+    {
+        private PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = portfolioManager.TGetList();
+            return View(values);
+        }
+    }
+}
