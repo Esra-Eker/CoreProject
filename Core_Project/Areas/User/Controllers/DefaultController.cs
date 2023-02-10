4dd53506ff396164d8e7b0b5fde813f8667c3728
Esra <elifesraeker@gmail.com>
2023-02-10 12:05:05 +0300

    Area eklendi ve configurasyonu yapıldı

---
+++ Core_Project/Areas/User/Controllers/DefaultController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.Areas.User.Controllers
+{
+    public class DefaultController : Controller
+    {
+        [Area("User")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
