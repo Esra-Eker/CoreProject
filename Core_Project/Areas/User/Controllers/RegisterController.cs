576ca95ba58ad5ea4e499a211a1cbe9899af8548
Esra <elifesraeker@gmail.com>
2023-02-10 17:07:10 +0300

    Register formu

---
+++ Core_Project/Areas/User/Controllers/RegisterController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.Areas.User.Controllers
+{
+    [Area("User")]
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string p)
+        {
+            return View();
+        }
+    }
+}
