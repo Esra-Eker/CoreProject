ce2747c2a2689079879322c0be360ef97ac8ca79
Esra <elifesraeker@gmail.com>
2023-02-14 15:09:41 +0300

    Login teması

---
+++ Core_Project/Areas/User/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.Areas.User.Controllers
+{
+    [Area("User")]
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<WriterUser> _signInManager;
+
+        public LoginController(SignInManager<WriterUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Core_Project/Areas/User/Models/UserLoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core_Project.Areas.User.Models
+{
+    public class UserLoginViewModel
+    {
+        [Display(Name = "Kullanıcı Adı")]
+        [Required(ErrorMessage = "Kullanıcı adını girin.")]
+        public string Username { get; set; }
+
+        [Display(Name = "Şifre")]
+        [Required(ErrorMessage = "Şifreyi girin.")]
+        public string Password { get; set; }
+    }
+}
